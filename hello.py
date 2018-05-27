96e53db0e0f80cd73eba60bf010a5b036724cfd8
ILikePythonAndDjango <nik1995osipov@gmail.com>
2018-05-27 11:21:32 +0300

    replace mmethod

--- hello.py
+++ hello.py
@@ -2,22 +2,12 @@
 # -*- coding: utf-8 -*-
 
 def wsgi_application(environ, start_response):
-    body = [bytes(i + '\n','utf-8') for i in environ['QUERY_STRING'].split('&')]
+    data = environ["QUERY_STRING"].replace('&', '\n').encode('utf-8')
     status = '200 OK'
     response_headers = [
         ('Content-Type', 'text/plain'),
-        ('Content-Length', str(len(body)))
+        ('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
-    
-    return body
 
-    #return map(
-    #    #Encoding strings in unicode
-    #    lambda string: string.encode("utf-8"),
-    #    map(
-    #        #make array with properties
-    #        lambda string: string + "\n",
-    #        environ["QUERY_STRING"].split("&")
-    #    )
-    #)
+    return iter([data])
