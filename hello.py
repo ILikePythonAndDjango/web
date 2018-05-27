e5f01132ea5c85728cbbc63c0019e9b917427c6e
ILikePythonAndDjango <nik1995osipov@gmail.com>
2018-05-27 10:51:48 +0300

    hello.py

--- hello.py
+++ hello.py
@@ -2,19 +2,22 @@
 # -*- coding: utf-8 -*-
 
 def wsgi_application(environ, start_response):
-    data = b"It's worked!"
+    body = [bytes(i + '\n','utf-8') for i in environ['QUERY_STRING'].split('&')]
     status = '200 OK'
     response_headers = [
         ('Content-Type', 'text/plain'),
-        ('Content-Length', str(len(data)))
+        ('Content-Length', str(len(body)))
     ]
     start_response(status, response_headers)
-    return map(
-        #Encoding strings in unicode
-        lambda string: string.encode("utf-8"),
-        map(
-            #make array with properties
-            lambda string: string + "\n",
-            environ["QUERY_STRING"].split("&")
-        )
-    )
+    
+    return body
+
+    #return map(
+    #    #Encoding strings in unicode
+    #    lambda string: string.encode("utf-8"),
+    #    map(
+    #        #make array with properties
+    #        lambda string: string + "\n",
+    #        environ["QUERY_STRING"].split("&")
+    #    )
+    #)
