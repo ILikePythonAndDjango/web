86dc5ce42ef042c56e24fc79339ec6a320399e4f
ILikePythonAndDjango <nik1995osipov@gmail.com>
2018-05-28 19:20:17 +0300

    make init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf $(pwd)/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -c etc/hello.py hello:wsgi_application
-gunicorn -c etc/application.py ask.wsgi:application
+#source core/bin/activate
+gunicorn -c etc/hello.py --pythonpath $(pwd)/ hello:wsgi_application  
+gunicorn -c etc/gunicorn.py --pythonpath $(pwd)/ask/ ask.wsgi:application
